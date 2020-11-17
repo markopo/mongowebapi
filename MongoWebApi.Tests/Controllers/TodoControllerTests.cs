6944534c23355cc50705265040345fd80ae14133
Marko <marko_poikkimaki@hotmail.com>
2020-11-17 21:21:08 +0100

    add test: TestGetListSuccess

--- MongoWebApi.Tests/Controllers/TodoControllerTests.cs
+++ MongoWebApi.Tests/Controllers/TodoControllerTests.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Globalization;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MongoWebApi.Controllers;
@@ -42,20 +44,61 @@ namespace MongoWebApi.Tests.Controllers
         [Fact]
         public async Task TestGetFailed()
         {
-
             var mockRepo = new Mock<ITodoRepository>();
             
             mockRepo
                 .Setup(x => x.GetTodo(11000))
-                .Returns(Task.FromResult<Todo>(null));
+                .Returns( Task.FromResult<Todo>(null));
             
             var controller = new TodoController(mockRepo.Object);
 
             var result = await controller.Get(11000);
 
             Assert.IsType<NotFoundResult>(result.Result);
-
         }
-        
+
+
+        [Fact]
+        public async Task TestGetListSuccess()
+        {
+            var mockRepo = new Mock<ITodoRepository>();
+            
+            mockRepo
+                .Setup(x => x.GetAllTodos())
+                .ReturnsAsync(new List<Todo>
+                {
+                    new Todo
+                    {
+                        Id = 11,
+                        Content = "random string",
+                        Title = "random title"
+                    },
+                    new Todo
+                    {
+                        Id = 12,
+                        Content = "random string 2",
+                        Title = "random title 2"
+                    },
+                    new Todo
+                    {
+                        Id = 13,
+                        Content = "random string 3",
+                        Title = "random title 3"
+                    }
+                });
+            
+            var controller = new TodoController(mockRepo.Object);
+
+            var result = await controller.Get();
+
+            Assert.NotNull(result);
+
+            var objResult = result.Result as ObjectResult;
+            var listResult = objResult.Value as List<Todo>;
+            
+            Assert.Equal(3, listResult.Count);
+            
+            
+        }
     }
 }
